b56b658c1d64159fbc7926b66184584301b84b8b
cuterinrin <yro7045@naver.com>
2018-12-11 13:59:13 +0900

    country 문자열 비교, 디버깅

--- movie_functionPointer/movie.c
+++ movie_functionPointer/movie.c
@@ -4,88 +4,90 @@
 #include "movie.h"
 #define mov 10
 //structure definition
+
+#if 1
 typedef struct movInfo {
 	char name[100];
 	float score;
 	int runTime;
 	char madeIn[10];
-} movInfo_t;
-movInfo[100];
+} movInfo_t[100];
 
-void* mv_genMvInfo(char* name, float score, int runTime, char* country)
-{
-	movInfo_t* mvPtr;
-	
-	//allocate memory and set the member variables
-	
-	return (void*)mvPtr;
-}
 
-void printMv(void* obj)
-{
-	movInfo_t* mvPtr = (movInfo_t*)obj;
-	if (mvPtr == NULL)
-	{
-		printf("[ERROR] failed to print the movie Info! (object is NULL)\n");
-	}
-	
-	printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn);
-	printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score);
-	
-	return;
-}
-
-int mv_printAll(void* obj, void* arg)
+int mv_printAll(void *mv)
 {	
-	int i=0;//for에서 쓸 i 
+	int i=0;
+	FILE *fp;
+	 
 	//파일열기 
 	fp=fopen("movie.txt","r");	
 
 	for(i=0;i<mov;i++){//구조체 배열에 각각 영화정보 넣기 
-		fscanf(fp,"%s %s %i %f", &movInfo_t.name[i], &movInfo_t.madeIn[i], &movInfo_t.runTime[i], &movInfo_t.score[i]);
+		fscanf(fp,"%s %s %i %f", &movInfo_t[i].name, &movInfo_t[i].madeIn, &movInfo_t[i].runTime, &movInfo_t[i].score);
 		}
 
 	for(i=0;i<mov;i++){//각각 프린트 
 		printf("--------------------------------------------\n");
-		printf("NAME : %s (%s) \nRunning Time : %i, Score : %3f\n", &movInfo_t.name[i], &movInfo_t.madeIn[i], &movInfo_t.runTime[i], &movInfo_t.score[i]);
+		printf("NAME : %s (%s) \nRunning Time : %i, Score : %3f\n", movInfo_t[i].name, movInfo_t[i].madeIn, movInfo_t[i].runTime, movInfo_t[i].score);
 	}
 
-	fclose(fp);//파일닫기 
+	fclose(fp);//파일닫기 	
+}
 
-
+int mv_printScore(void *mv)
+{	int cnt=0;
+	int i =0;
+	float input_score;
 	
-}
+	printf("lowest score : ");
+	scanf("%s", &input_score);
 
-int mv_printScore(void* obj, void* arg)
-{
 	for(i=0;i<mov;i++){
-		if(movInfo_t.score[i]>score){	
+		if(movInfo_t.score[i]>input_score){	
 			printf("--------------------------------------------\n");
-			printf("NAME : %s (%s) \nRunning Time : %i, Score : %3f\n", &movInfo_t.name[i], &movInfo_t.madeIn[i], &movInfo_t.runTime[i], &movInfo_t.score[i]);
+			printf("NAME : %s (%s) \nRunning Time : %i, Score : %3f\n", movInfo_t.name[i], movInfo_t.madeIn[i], movInfo_t.runTime[i], movInfo_t.score[i]);
+			cnt++;
 		}
 	}
+	printf("totaly %i movies are listed!", cnt);
 }
 
-int mv_printRunTime(void* obj, void* arg)
-{
-		for(i=0;i<mov;i++){
-		if(movInfo_t.runTime[i]>runTime){	
+int mv_printRunTime(void* mv)
+{	int cnt=0;
+	int i =0;
+	int input_run;
+
+	printf("lowest runtime : ");
+	scanf("%s", &input_run);
+	
+	for(i=0;i<mov;i++){
+		if(movInfo_t.runTime[i]>input_run){	
 			printf("--------------------------------------------\n");
-			printf("NAME : %s (%s) \nRunning Time : %i, Score : %3f\n", &movInfo_t.name[i], &movInfo_t.madeIn[i], &movInfo_t.runTime[i], &movInfo_t.score[i]);
+			printf("NAME : %s (%s) \nRunning Time : %i, Score : %3f\n", movInfo_t.name[i], movInfo_t.madeIn[i], movInfo_t.runTime[i], movInfo_t.score[i]);
+			cnt++;
 		}
 	}
+	printf("totaly %i movies are listed!", cnt);
 }
 
-int mv_printCountry(void* obj, void* arg)
-{
-		for(i=0;i<mov;i++){
-		if(movInfo_t.madeIn[i] == country){	
+int mv_printCountry(void* mv)
+{	int cnt=0;
+	int i =0;
+	char input_con;
+	 
+	printf("select country : ");
+	scanf("%s", &input_con);
+		
+	for(i=0;i<mov;i++){
+		if(strncmp(input_con, movInfo_t[i].madeIn, strlen(input_con)) == 0){	//string.h dlttdma - 한글자씩 읽는걸로 대체 
 			printf("--------------------------------------------\n");
-			printf("NAME : %s (%s) \nRunning Time : %i, Score : %3f\n", &movInfo_t.name[i], &movInfo_t.madeIn[i], &movInfo_t.runTime[i], &movInfo_t.score[i]);
+			printf("NAME : %s (%s) \nRunning Time : %i, Score : %3f\n", movInfo_t.name[i], movInfo_t.madeIn[i], movInfo_t.runTime[i], movInfo_t.score[i]);
+			cnt++;
 		}
 	}
+	printf("totaly %i movies are listed!", cnt);
 }
 
-
+#endif
 
 
